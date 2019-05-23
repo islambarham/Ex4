00d940e6dc6888d00b025f91a91566c63a61a884
islam barham <islambarham7@gmail.com>
2019-05-24 02:06:55 +0300

    Ex4.working module

---
+++ loginp.php
@@ -0,0 +1,13 @@
+<!doctypy html>
+<html>
+    <head>
+        <title>forms</title>
+    </head>
+<body>
+    <?php
+    if($_get["reg_un"]=="Islam" && $_get["reg_pas"]=="1997" )
+    {echo '<h1>Welcome Islam</h1><h2>You are in your page</h2>';}
+    else
+    {echo '<h1>Welcome Human</h1><h2>You are in Erorr page</h2><h3>Please go out</h3>';}?>
+</body>
+</html>
